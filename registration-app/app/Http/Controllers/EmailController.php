4d7a0f7f587a5203ddb18c23fe727c4b2d7ab83f
fadiakhaled <110059717+fadiakhaledd@users.noreply.github.com>
2023-05-13 23:47:26 +0300

    Sending an email to the system admin upon a new user registration

---
+++ registration-app/app/Http/Controllers/EmailController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\emailMailable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Routing\Controller as BaseController;
+
+
+class EmailController extends BaseController
+{
+    public function send($username)  {
+        Mail::to('system_admin@gmail.com')->send(new emailMailable($username));
+    }
+}
---
+++ registration-app/resources/views/email.blade.php
@@ -0,0 +1,4 @@
+<h1>New Registered User</h1>
+<p>
+    A new user {{$username}} is registered to the system
+</p>
